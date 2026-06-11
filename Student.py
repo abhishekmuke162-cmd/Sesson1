bf70bb5dc015ab14a90972f8ff936790cf250f7d
abhishekmuke162-cmd <abhishekmuke162@gmail.com>
2026-06-11 13:52:03 +0530

    Add Student class with methods and attributes

---
+++ Student.py
@@ -0,0 +1,22 @@
+# class example
+
+class student:# class name
+    fname = 'abhi'
+    iname = 'girase'
+    sub = 'computer'
+    marks = 67.66
+
+    def reading(self):
+     print('student name is reading the book')
+
+        
+    def playing(self):
+     print('student name is playing the cricket' )
+
+objstudent=student()
+print(objstudent.fname)
+print(objstudent.iname)
+print(objstudent.sub)
+print(objstudent.marks)
+objstudent.reading()
+objstudent.playing()
