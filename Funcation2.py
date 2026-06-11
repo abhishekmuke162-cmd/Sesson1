b3de1b006db0dcd28a82516caf08e0bc2904f829
abhishekmuke162-cmd <abhishekmuke162@gmail.com>
2026-06-11 13:07:59 +0530

    Implement deposit, withdraw, and balance functions

---
+++ Funcation2.py
@@ -0,0 +1,10 @@
+def deposit(amount):
+    print("Dposit Amount",amount)
+def withdraw(amount):
+    print("Withdraw Amount",amount)
+def balance(amount):
+    print ("balance:",amount)
+
+deposit(1000)
+withdraw(500)
+balance(50000)
