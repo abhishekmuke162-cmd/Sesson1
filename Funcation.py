345d852fc55fd066c060842d1d1a388d9c1513c4
abhishekmuke162-cmd <abhishekmuke162@gmail.com>
2026-06-11 13:07:14 +0530

    Add basic ATM functions in Funcation.py

---
+++ Funcation.py
@@ -0,0 +1,20 @@
+def welcome():
+    print("welcome to Bank")
+
+def deposit():
+    print("Amount Deposited: 1000")
+
+def withdraw():
+    print("Amount Withdeaw: 500")
+
+def balance():
+    print("Balance:5000")
+
+def application():
+    print("Application for ATM card")
+
+welcome()
+deposit()
+withdraw()
+balance()
+application()
