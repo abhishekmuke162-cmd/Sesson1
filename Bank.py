0a078f0ca09aa1e2a165c0ad03e8ba6c9f65a17b
abhishekmuke162-cmd <abhishekmuke162@gmail.com>
2026-06-11 12:57:35 +0530

    Add initial Bank.py with account details display

---
+++ Bank.py
@@ -0,0 +1,21 @@
+print('======Bank======.')
+account_no=2006
+name="muke"
+mobile="9021376442"
+email="abhimuke@gmail.com"
+address="dolasane"
+account_type ="saving" 
+balance=45000.00
+
+
+print("n\======ACCOUNT DETAILS======")
+print("Account Number :",account_no)
+print("Customer Name :",name)
+print("Mobile Noumber:",mobile)
+print("Email ID :",email)
+print("Address :",address)
+print("Account Type :",account_type)
+print("Balance :",balance)
+
+
+print("\naccount created successfully!")
