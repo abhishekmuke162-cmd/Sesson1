9decff3cda8b93bf736ca4ba7d73f534c160da8b
abhishekmuke162-cmd <abhishekmuke162@gmail.com>
2026-06-11 13:21:16 +0530

    Implement basic login functionality

---
+++ Loging.py
@@ -0,0 +1,8 @@
+username="abhishek"
+password="pass1234"
+if(username== "abhishek" and password=="abhi1234"):
+    print("wealcome abhi")
+elif username=="admin" and password=="admin123":
+    print("Welcome Admin")
+else:
+    print("invalid loging")
