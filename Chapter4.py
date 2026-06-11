4a1d68f6b1a65b4af74fb1a7bc7e07053c7a2f47
abhishekmuke162-cmd <abhishekmuke162@gmail.com>
2026-06-11 12:59:42 +0530

    Add loops with break, continue, and pass examples

---
+++ Chapter4.py
@@ -0,0 +1,19 @@
+for i in range(5):
+    if i==3:
+
+      break # exit loop at i=3
+    print(i)
+
+for i in range(5):
+     if i ==2:
+    
+      continue # skip priting when i=2
+     print(i)
+
+for i in range(5):  
+  if i < 3:
+     pass # dose nothing here
+print(i)
+
+for i in range(1,11):
+    print(i)
