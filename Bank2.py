060a8ba0cfad1af649f29b1bd33427584c2bb142
abhishekmuke162-cmd <abhishekmuke162@gmail.com>
2026-06-11 12:58:26 +0530

    Add initial bank application form in Bank2.py

---
+++ Bank2.py
@@ -0,0 +1,9 @@
+print("======BANK APPLICATION FORM======")
+
+account_no=1001
+name="abhishek"
+mobile="8805333981"
+email="abhi123@gmail.com"
+address="sangamner"
+account_type="saving"
+balance=5000.00
