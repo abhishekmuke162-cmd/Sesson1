98e3899de700c591d756f6b5e9b5eb7fa8d91b48
abhishekmuke162-cmd <abhishekmuke162@gmail.com>
2026-06-11 13:30:04 +0530

    Correct variable assignments and output formatting
    Fix variable assignments and print statements.

---
+++ Variables.py
@@ -0,0 +1,14 @@
+# Assiging values to variables
+x=5
+y=3
+
+# Adding the variables
+result=x+y
+
+# printing the output
+print("The value of x is:", x)
+print("The value of y is:", y)
+print("5 + 3 =". result)
+print("8 + 9 =". rusult)
+print("8 + 9 =". rusult)
+print("5 + 5 =". rusult)
