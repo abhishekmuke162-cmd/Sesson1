6a195829644ef9c10a9183c4740b2f519bdf4a39
abhishekmuke162-cmd <abhishekmuke162@gmail.com>
2026-06-13 21:40:44 +0530

    Create oopsclass and object.py

---
+++ oopsclass and object.py
@@ -0,0 +1,14 @@
+ # Defining a class named Bike
+class Bike:
+    name = ""
+    gear = 0
+
+# Creating an instance of the Bike class
+bike1 =  Bike()
+
+# Assing values to the object properties
+bike1.name = "Mountain Bike"
+bike1.gear = 11 
+
+#Access obkject properties
+print(f"Name: {bike1.name}, Gear: {bike1.gear}")
